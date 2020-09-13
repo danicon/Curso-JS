6ad39c6bfc6758bf1c05c23b6be9a110bf5aa3aa
Danicon <dancon.alferes@gmail.com>
2020-09-13 12:35:23 -0300

    Material do Daniel

---
+++ Dn-01/Dia_do_Programador.js
@@ -0,0 +1,9 @@
+const data = new Date()
+var Data = data.getDate(12)
+const DiadoProgramador = new Date(13)
+
+if (Data >= DiadoProgramador) {
+    console.log("Parabéns! Feliz dia dos Programadores!")
+} else {
+    console.log("Ainda não é dia dos Programadores! Vai tomar Café!")
+}
---
+++ Dn-01/calc.binario.js
@@ -0,0 +1,6 @@
+let entrada = Number(11);
+
+console.log(entrada + " Decimal: " + entrada);
+console.log(entrada + " Binário: " + entrada.toString(2));
+console.log(entrada + " Ócta: " + entrada.toString(8));
+console.log(entrada + " Hexadecimal: " + entrada.toString(16));
